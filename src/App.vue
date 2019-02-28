f377fecbf8b6960081f71dfbcf956e62d40cb63c
66lulu <huangl@133.cn>
2019-02-28 18:18:10 +0800

    add my-study

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <ramda></ramda>
+  </div>
+</template>
+
+<script>
+  import Ramda from './ramda/index.vue'
+
+  export default {
+    name: 'app',
+    components: {
+      Ramda
+    }
+  }
+</script>
+
+<style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
